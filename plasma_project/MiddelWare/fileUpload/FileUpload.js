7df7e74dfd62f525eec5246899325d820774b9e5
Rahul-ai <rt878777@gmail.com>
2023-01-18 20:01:31 +0530

    Added dep

---
+++ plasma_project/MiddelWare/fileUpload/FileUpload.js
@@ -0,0 +1,24 @@
+const multer = require('multer');
+const uuid = require("uuid").v4;
+// const path = require('path');
+
+const storage = multer.diskStorage({
+    destination:(req,file, cb) =>{
+        cb(null,'Images')
+    },
+    filename: (req, file, cb) =>{
+        const {originalname} = file
+        cb(null,`${uuid()}-${originalname}`);
+    }
+});
+const upload = multer({storage:storage});
+// const upload = multer({dest: './Images'});
+const multiUpload = upload.fields([
+    {name:"file1", maxCount:1},
+    {name:"file2", maxCount:1},
+    {name:"file3", maxCount:1},
+    {name:"file4", maxCount:1},
+    {name:"file5", maxCount:1},
+])
+
+module.exports = {upload, multiUpload};
