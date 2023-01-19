c59b176a5012a21641039dfe87a70f2236878346
Rahul-ai <rt878777@gmail.com>
2023-01-19 12:16:06 +0530

    clean code

--- plasma_project/Controller/UserController.js
+++ plasma_project/Controller/UserController.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const {prisma} = require('../Configuration/Database')
 
-router.get("/", (req, res)=>{
+
+router.get("/", async(req, res)=>{ 
     res.send("hello Plasma");
 })
 
