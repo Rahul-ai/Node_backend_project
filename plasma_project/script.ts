4cecd471819b8bcbb62fd7a3eb346801081588ea
Rahul-ai <rt878777@gmail.com>
2023-01-20 11:41:57 +0530

    clean code

--- plasma_project/script.ts
+++ plasma_project/script.ts
@@ -3,26 +3,26 @@ const prisma = new PrismaClient({ log : ["query"]})
 
 
 async function main() {
-    // await prisma.user.deleteMany();
-    // const user = await prisma.user.create({
-    //     data:
-    //     {   
-    //         name:"Rahul",
-    //         email:'rt878777@gmail.com',
-    //         posts:
-    //         {
-    //             create:
-    //             {
-    //                 post:"Hello world",
-    //             }
-    //         },
-    //     }
-    // });  
-    const user = await prisma.user.findMany({
-        include: {
-            posts: true,
+    await prisma.user.deleteMany();
+    const user = await prisma.user.create({
+        data:
+        {   
+            name:"Rahul",
+            email:'rt878777@gmail.com',
+            posts:
+            {
+                create:
+                {
+                    post:"Hello world",
+                }
+            },
         }
-    })
+    });  
+    // const user = await prisma.user.findMany({
+    //     include: {
+    //         posts: true,
+    //     }
+    // })
     console.log(user);
 
 }
