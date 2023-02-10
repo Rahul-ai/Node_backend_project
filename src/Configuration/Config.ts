c7c41e2f15f6c5a5aa6015d380108fee294944fa
Rahul-ai <rt878777@gmail.com>
2023-02-10 16:49:21 +0530

    clean code

--- src/Configuration/Config.ts
+++ src/Configuration/Config.ts
@@ -1,10 +1,4 @@
 // Port configutation
-
-interface config {
-    port : number;
-    Api : string;
-}
-
 const config :config = {
     port:3001,
     Api:'http://localhost:3009/',
---
+++ src/healper/configModel.ts
@@ -0,0 +1,4 @@
+interface config {
+    port : number;
+    Api : string;
+}
