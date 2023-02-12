001f8df6a0cf8346682e35aedc709196886c8763
Rahul-ai <rt878777@gmail.com>
2023-02-12 18:46:02 +0530

    clean code

--- src/Controller/UserController.ts
+++ src/Controller/UserController.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { controllerService } from "../ControllerService/ControllerService";
 import { User } from "../entity/User";
+import { db } from "../Configuration/dbConfig";
 const router = express.Router();
 
 // Crud Router
