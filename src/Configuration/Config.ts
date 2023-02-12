8c5d00b38d57e59b686736395b5848639aea577b
Rahul-ai <rt878777@gmail.com>
2023-02-12 17:54:26 +0530

    clean code

--- src/Configuration/Config.ts
+++ src/Configuration/Config.ts
@@ -1,6 +1,6 @@
 import { configModel } from "../Modelhealper/configModel";
-// Port configutation
 
+// Port configutation
 const config : configModel = {
     port:3001,
     Api:'http://localhost:3001',
