3822d6be8fd7cd6aea828d6d706d47a6cd4278b8
Rahul-ai <rt878777@gmail.com>
2023-02-12 11:14:23 +0530

    addded create repo

--- src/GenericRepo/GRepo.ts
+++ src/GenericRepo/GRepo.ts
@@ -27,6 +27,16 @@ class GRepo
          res.status(500).json(e);
       }
    }
+
+   public async create(req: Request, res: Response) {
+      try {
+         const data = await db.manager.create(entity, req.body);
+         res.status(200).json(data);
+      }
+      catch (e) {
+         res.status(500).json(e);
+      }
+   }
 }
 return new GRepo();
 
